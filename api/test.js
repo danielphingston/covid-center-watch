12f35fbae62eabf9743ed21e61f7bbb4e4d30b77
daniel <daniel.p@gadgeon.com>
2021-05-09 13:07:22 +0530

    try covid api

--- api/test.js
+++ api/test.js
@@ -1,7 +1,10 @@
+const { default: fetch } = require("node-fetch");
+
 module.exports = (req, res) => {
+    const b=await fetch(
+        "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=512&date=31-03-2021"
+    )
     res.json({
-      body: req.body,
-      query: req.query,
-      cookies: req.cookies,
-    })
-  }
+        body: b
+    });
+};
