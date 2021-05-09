7f5074477c78f0fa54a9484931f0f44be50e48de
daniel <daniel.p@gadgeon.com>
2021-05-09 13:10:32 +0530

    added async

--- api/test.js
+++ api/test.js
@@ -1,10 +1,10 @@
 const { default: fetch } = require("node-fetch");
 
-module.exports = (req, res) => {
-    const b=await fetch(
+module.exports = async (req, res) => {
+    const b = await fetch(
         "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=512&date=31-03-2021"
-    )
+    );
     res.json({
-        body: b
+        body: b,
     });
 };
