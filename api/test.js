348f39aba7bbf23a6d3cc1b647752a119bf977ac
daniel <daniel.p@gadgeon.com>
2021-05-09 13:25:30 +0530

    trying with host

--- api/test.js
+++ api/test.js
@@ -5,7 +5,8 @@ module.exports = async (req, res) => {
         "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=512&date=31-03-2021",
         {
             headers: {
-                Host: "https://www.cowin.gov.in",
+                referer: "https://www.cowin.gov.in/",
+                origin: "https://www.cowin.gov.in",
                 "user-agent":
                     "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36",
             },
