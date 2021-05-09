9b789d9fd0585957a4049e2b6d67dc2254dd705e
daniel <daniel.p@gadgeon.com>
2021-05-09 13:12:37 +0530

    fixed api

--- api/test.js
+++ api/test.js
@@ -4,6 +4,7 @@ module.exports = async (req, res) => {
     const b = await fetch(
         "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=512&date=31-03-2021"
     );
+    b = await b.text();
     res.json({
         body: b,
     });
